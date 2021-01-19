6c8da545020f93ffcf8e1b015235342380d64961
Davi Coscarelli <davicoscarelli@gmail.com>
2021-01-19 19:37:22 -0400

    feat: chocking

--- bot.js
+++ bot.js
@@ -1,23 +1,47 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
 
 const { ActivityHandler, MessageFactory } = require('botbuilder');
+import choking from './messages/choking.json'
 
-class EchoBot extends ActivityHandler {
+class USafe extends ActivityHandler {
     constructor() {
         super();
-        // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
+            
+            let input = JSON.parse(context.activity.text)
+            let text = ''
 
-            const replyText = `Echo aa: ${ JSON.stringify(context) }`;
-            await context.sendActivity(MessageFactory.text(replyText, replyText));
-            // if (context.activity.text === 'ralado'){
-            //     await context.sendActivity(MessageFactory.text('passa metiolate', 'passa metiolate'));
-            // }
+            if (input[0] === 0){
+                if (input === [0,0]) text = choking.c0
+                else if (input === [0,0,0]) text = choking.c00
+                else if (input === [0,0,1]) text = choking.c01
+                else if (input === [0,0,1,0]) text = choking.c010
+                else if (input === [0,0,1,1]) text = choking.c011
+                else if (input === [0,0,0,1]) text = choking.c001
+                else if (input === [0,0,0,0]) text = choking.c000
+                else if (input === [0,0,1,0,0]) text = choking.c0100
+                else if (input === [0,0,1,0,1]) text = choking.c0101
+                else if (input === [0,0,1,1,0]) text = choking.c0110
+                else if (input === [0,0,1,1,1]) text = choking.c0111
+                else if (input === [0,0,1,1,1,0]) text = choking.c01110
+                else if (input === [0,0,1,1,1,1]) text = choking.c01111
 
-            //await context.sendActivity(MessageFactory.text(replyText, replyText));
+                else if (input === [0,1]) text = choking.c1
+                else if (input === [0,1,1,0]) text = choking.c110
+                else if (input === [0,1,1,0]) text = choking.c111
+                else if (input === [0,1,0,1]) text = choking.c101
+                else if (input === [0,1,0,0]) text = choking.c100
+                else if (input === [0,1,1,0,0]) text = choking.c1100
+                else if (input === [0,1,1,0,1]) text = choking.c1101
+            
+            }else if (input[0] === 1){
+                text = 'other.1'
+            }
+
+            await context.sendActivity(MessageFactory.text(text))
+
             await next();
         });
+
 
         this.onMembersAdded(async (context, next) => {
             const membersAdded = context.activity.membersAdded;
@@ -33,5 +57,5 @@ class EchoBot extends ActivityHandler {
     }
 }
 
-module.exports.EchoBot = EchoBot;
+module.exports.USafe = USafe;
 
