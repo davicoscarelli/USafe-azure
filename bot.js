80cdb124f9eb059ae6eef50eebf3ac78aa26e36c
Davi Coscarelli <davicoscarelli@gmail.com>
2021-01-19 21:21:12 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,39 +9,40 @@ class EchoBot extends ActivityHandler {
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
 
-            // if (input[0] === 0){
-                //     if (input === [0,0]) text = choking.c0
-                //     else if (input === [0,0,0]) text = choking.c00
-                //     else if (input === [0,0,1]) text = choking.c01
-                //     else if (input === [0,0,1,0]) text = choking.c010
-                //     else if (input === [0,0,1,1]) text = choking.c011
-                //     else if (input === [0,0,0,1]) text = choking.c001
-                //     else if (input === [0,0,0,0]) text = choking.c000
-                //     else if (input === [0,0,1,0,0]) text = choking.c0100
-                //     else if (input === [0,0,1,0,1]) text = choking.c0101
-                //     else if (input === [0,0,1,1,0]) text = choking.c0110
-                //     else if (input === [0,0,1,1,1]) text = choking.c0111
-                //     else if (input === [0,0,1,1,1,0]) text = choking.c01110
-                //     else if (input === [0,0,1,1,1,1]) text = choking.c01111
-
-                //     else if (input === [0,1]) text = choking.c1
-                //     else if (input === [0,1,1,0]) text = choking.c110
-                //     else if (input === [0,1,1,0]) text = choking.c111
-                //     else if (input === [0,1,0,1]) text = choking.c101
-                //     else if (input === [0,1,0,0]) text = choking.c100
-                //     else if (input === [0,1,1,0,0]) text = choking.c1100
-                //     else if (input === [0,1,1,0,1]) text = choking.c1101
-                
-                // }else if (input[0] === 1){
-                //     text = 'other.1'
-                // }
-
             const input = JSON.parse(context.activity.text)
-            let text = 'choking.c0'
+            let text = ''
 
             if (input[0] === 0){
-                await context.sendActivity(MessageFactory.text(text, text));
-            }
+                    if (input === [0,0]) text = "A vítima está com dificuldade para respirar?"
+                    else if (input === [0,0,0]) text = "A vítima tem mais de 8 anos de idade?"
+                    else if (input === [0,0,1]) text = "A vítima está consciente?"
+                    else if (input === [0,0,1,0]) text = "A vítima tem mais de 8 anos de idade??"
+                    else if (input === [0,0,1,1]) text = "A vítima tem mais de 8 anos de idade??"
+                    else if (input === [0,0,0,1]) text = "ANSWER1"
+                    else if (input === [0,0,0,0]) text = "ANSWER2"
+                    else if (input === [0,0,1,0,0]) text = "ANSWER6"
+                    else if (input === [0,0,1,0,1]) text = "ANSWER5"
+                    else if (input === [0,0,1,1,0]) text = "ANSWER2"
+                    else if (input === [0,0,1,1,1]) text = "A vítima é Grávida/Obesa?"
+                    else if (input === [0,0,1,1,1,0]) text = "ANSWER3"
+                    else if (input === [0,0,1,1,1,1]) text = "ANSWER4"
+
+                    else if (input === [0,1]) text = "Você está com dificuldade para respirar?"
+                    else if (input === [0,1,1,0]) text = "Você está sozinho(a)?"
+                    else if (input === [0,1,1,0]) text = "ANSWER3.2"
+                    else if (input === [0,1,0,1]) text = "ANSWER1"
+                    else if (input === [0,1,0,0]) text = "ANSWER2+"
+                    else if (input === [0,1,1,0,0]) text = "ANSWER2+"
+                    else if (input === [0,1,1,0,1]) text = "CALL 911"
+                
+                }else if (input[0] === 1){
+                    text = 'other.1'
+                }
+
+            
+            
+            await context.sendActivity(MessageFactory.text(text, text));
+            
 
             //await context.sendActivity(MessageFactory.text(replyText, replyText));
             await next();
